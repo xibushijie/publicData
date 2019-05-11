b7bdc47f9cb2ea348b482a24b7525e7a0ad40efe
TTX <962325513@qq.com>
2019-05-11 21:42:55 +0800

    ttx
    菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
-    <el-container class="main">
-        <el-header>公共数据-绩效评估</el-header>
-        <el-container>
+  <el-container class="main">
+    <el-header>公共数据-绩效评估</el-header>
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <!-- <el-container>
             <el-aside width="200px">
                 <el-menu background-color="#38484D" text-color="#fff">
                     <el-menu-item>
@@ -18,29 +19,94 @@
             <el-main>
                 <router-view/>
             </el-main>
-        </el-container>
-    </el-container>
+    </el-container>-->
+  </el-container>
 </template>
 
 <script type="text/javascript">
 export default {
-    data() {
-        return {};
-    },
-    mounted() {},
-    methods: {}
+  data() {
+    return {
+      data: [
+        {
+          label: "指标管理"
+          //   children: [
+          //     {
+          //       label: "二级 1-1",
+          //       children: [
+          //         {
+          //           label: "三级 1-1-1"
+          //         }
+          //       ]
+          //     }
+          //   ]
+        },
+        {
+          label: "考核管理"
+          //   children: [
+          //     {
+          //       label: "二级 2-1",
+          //       children: [
+          //         {
+          //           label: "三级 2-1-1"
+          //         }
+          //       ]
+          //     },
+          //     {
+          //       label: "二级 2-2",
+          //       children: [
+          //         {
+          //           label: "三级 2-2-1"
+          //         }
+          //       ]
+          //     }
+          //   ]
+        },
+        {
+          label: "考核结果"
+          //   children: [
+          //     {
+          //       label: "二级 3-1",
+          //       children: [
+          //         {
+          //           label: "三级 3-1-1"
+          //         }
+          //       ]
+          //     },
+          //     {
+          //       label: "二级 3-2",
+          //       children: [
+          //         {
+          //           label: "三级 3-2-1"
+          //         }
+          //       ]
+          //     }
+          //   ]
+        }
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label"
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    }
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .sideMenu {
-    li {
-        text-align: center;
-        height: 35px;
-        line-height: 35px;
-        &:hover {
-            background-color: #059ddb;
-        }
+  li {
+    text-align: center;
+    height: 35px;
+    line-height: 35px;
+    &:hover {
+      background-color: #059ddb;
     }
+  }
 }
 </style>
